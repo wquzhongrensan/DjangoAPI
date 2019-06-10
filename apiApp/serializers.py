8b979b6fece616fc7ac46a1c3ec3a6cffb7f88f6
ljp <19802021278@139.com>
2019-06-10 17:23:32 +0800

    增加serializer的使用：1.model转json.   2. 验证用户提交的数据  3.返回URL

--- apiApp/serializers.py
+++ apiApp/serializers.py
@@ -39,3 +39,31 @@ class ChoiceSerializer(serializers.ModelSerializer):
         model = Choice
         fields = ('content', 'question')
 
+
+# serializer有三个功能： 1.对象转json   2.验证用户提交的数据  3.在返回给客户端的数据中返回URL
+# 使用serializer 来验证 客户端提交的数据 演示
+# class UserModelSerializer(serializers.ModelSerializer):
+#     # ModelSerializer继承于Serializer,所以在里面Serializers中的语法仍然是可以使用的
+#     user_type = serializers.CharField(source='get_user_type_display')
+#     group = serializers.CharField(source='group.title')
+#     role = serializers.SerializerMethodField()
+#
+#     # get_字段名称；自定义显示字段实现需要定义这样的一个方法
+#     def get_role(self, obj):
+#         # obj就是UserInfo的对象  user.role.all()  /  role.user_set.all()
+#         roles = obj.role.all()
+#         results = []
+#         for role in roles:
+#             results.append({
+#                 'id': role.id,
+#                 'title': role.title
+#             })
+#         return results
+#
+#     class Meta:
+#         # 指定要序列化哪一个模型
+#         model = UserInfo
+#         # ModelSerializer类使用fields控制需要序列化的字段
+#         # fields = "__all__"
+#         fields = ('role','group','user_type','username')
+
