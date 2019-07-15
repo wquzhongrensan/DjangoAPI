0107fe625ed90ba471c0d7891127125277ebbd43
ljp <19802021278@139.com>
2019-07-15 17:11:07 +0800

    增加数据表

--- sentence/models.py
+++ sentence/models.py
@@ -1,3 +1,233 @@
+import logging
+from abc import ABCMeta, abstractmethod, abstractproperty
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+from uuslug import slugify
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from DjangoBlog.utils import get_current_site
+from DjangoBlog.utils import cache_decorator, cache
+from django.utils.functional import cached_property
+from django.utils.timezone import now
+from mdeditor.fields import MDTextField
+from django.db.models.signals import post_save
+
 
 # Create your models here.
+logger = logging.getLogger(__name__)
+
+
+LINK_SHOW_TYPE = (
+    ('i', '首页'),
+    ('l', '列表页'),
+    ('p', '文章页面'),
+    ('a', '全站'),
+)
+
+# 问文章相关的基础model
+class BaseModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_time = models.DateTimeField('创建时间', default=now)
+    last_mod_time = models.DateTimeField('修改时间', default=now)
+
+    def save(self, *args, **kwargs):
+        is_update_views = isinstance(self, Sentence) and 'update_fields' in kwargs and kwargs['update_fields'] == [
+            'views']
+        if is_update_views:
+            Sentence.objects.filter(pk=self.pk).update(views=self.views)
+        else:
+            if 'slug' in self.__dict__:
+                slug = getattr(self, 'title') if 'title' in self.__dict__ else getattr(self, 'name')
+                setattr(self, 'slug', slugify(slug))
+            super().save(*args, **kwargs)
+
+    def get_full_url(self):
+        site = get_current_site().domain
+        url = "https://{site}{path}".format(site=site, path=self.get_absolute_url())
+        return url
+
+    class Meta:
+        abstract = True
+
+    @abstractmethod
+    def get_absolute_url(self):
+        pass
+
+# 句子model 草稿和完成的句子都是在同一个表
+class Sentence(BaseModel):
+
+    """句子"""
+    STATUS_CHOICES = (
+        ('d', '草稿'),
+        ('p', '发表'),
+    )
+    COMMENT_STATUS = (
+        ('o', '打开'),
+        ('c', '关闭'),
+    )
+    TYPE = (
+        ('a', '文章'),
+        ('p', '页面'),
+    )
+
+    title = models.CharField('标题', max_length=200, unique=True)
+    # 文章内容 支持markdown语法
+    body = MDTextField('正文')
+    pub_time = models.DateTimeField('发布时间', blank=True, null=True)
+    status = models.CharField('文章状态', max_length=1, choices=STATUS_CHOICES, default='p')
+    comment_status = models.CharField('评论状态', max_length=1, choices=COMMENT_STATUS, default='o')
+    type = models.CharField('类型', max_length=1, choices=TYPE, default='a')
+    # 句子的浏览量
+    views = models.PositiveIntegerField('浏览量', default=0)
+    # 句子的撰写者
+    editor = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='作者', on_delete=models.CASCADE)
+    # 句子的排序
+    article_order = models.IntegerField('排序,数字越大越靠前', blank=False, null=False, default=0)
+    # 句子 是 归属在那一个类别上 人生、事业、爱情、理想等
+    category = models.ForeignKey('Category', verbose_name='分类', on_delete=models.CASCADE, blank=False, null=False)
+    #  句子的标签
+    tags = models.ManyToManyField('Tag', verbose_name='标签集合', blank=True)
+    # 原创作者
+    originator = models.CharField('原创作者', max_length=100)
+    # 是否是原创 还是 摘录于名言
+    is_original = models.CharField(max_length=1, null=True, verbose_name="是否原创句子")
+
+    def body_to_string(self):
+        return self.body
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-article_order', '-pub_time']
+        verbose_name = "文章"
+        verbose_name_plural = verbose_name
+        get_latest_by = 'id'
+
+    def get_absolute_url(self):
+        return reverse('blog:detailbyid', kwargs={
+            'article_id': self.id,
+            'year': self.created_time.year,
+            'month': self.created_time.month,
+            'day': self.created_time.day
+        })
+
+    # 使用缓存
+    @cache_decorator(60 * 60 * 10)
+    def get_category_tree(self):
+        tree = self.category.get_category_tree()
+        names = list(map(lambda c: (c.name, c.get_absolute_url()), tree))
+
+        return names
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def viewed(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    def comment_list(self):
+        cache_key = 'article_comments_{id}'.format(id=self.id)
+        value = cache.get(cache_key)
+        if value:
+            logger.info('get article comments:{id}'.format(id=self.id))
+            return value
+        else:
+            comments = self.comment_set.filter(is_enable=True)
+            cache.set(cache_key, comments, 60 * 100)
+            logger.info('set article comments:{id}'.format(id=self.id))
+            return comments
+
+    def get_admin_url(self):
+        info = (self._meta.app_label, self._meta.model_name)
+        return reverse('admin:%s_%s_change' % info, args=(self.pk,))
+
+    # 下一篇文章
+    @cache_decorator(expiration=60 * 100)
+    def next_article(self):
+        # 下一篇
+        return Sentence.objects.filter(id__gt=self.id, status='p').order_by('id').first()
+
+    # 上一遍文章
+    @cache_decorator(expiration=60 * 100)
+    def prev_article(self):
+        # 前一篇
+        return Sentence.objects.filter(id__lt=self.id, status='p').first()
+
+
+class Category(BaseModel):
+    """文章分类"""
+    name = models.CharField('分类名', max_length=30, unique=True)
+    parent_category = models.ForeignKey('self', verbose_name="父级分类", blank=True, null=True, on_delete=models.CASCADE)
+    slug = models.SlugField(default='no-slug', max_length=60, blank=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "分类"
+        verbose_name_plural = verbose_name
+
+    def get_absolute_url(self):
+        return reverse('blog:category_detail', kwargs={'category_name': self.slug})
+
+    def __str__(self):
+        return self.name
+
+    @cache_decorator(60 * 60 * 10)
+    def get_category_tree(self):
+        """
+        递归获得分类目录的父级
+        :return:
+        """
+        categorys = []
+
+        def parse(category):
+            categorys.append(category)
+            if category.parent_category:
+                parse(category.parent_category)
+
+        parse(self)
+        return categorys
+
+    @cache_decorator(60 * 60 * 10)
+    def get_sub_categorys(self):
+        """
+        获得当前分类目录所有子集
+        :return:
+        """
+        categorys = []
+        all_categorys = Category.objects.all()
+
+        def parse(category):
+            if category not in categorys:
+                categorys.append(category)
+            childs = all_categorys.filter(parent_category=category)
+            for child in childs:
+                if category not in categorys:
+                    categorys.append(child)
+                parse(child)
+
+        parse(self)
+        return categorys
+
+
+class Tag(BaseModel):
+    """文章标签"""
+    name = models.CharField('标签名', max_length=30, unique=True)
+    slug = models.SlugField(default='no-slug', max_length=60, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('blog:tag_detail', kwargs={'tag_name': self.slug})
+
+    @cache_decorator(60 * 60 * 10)
+    def get_article_count(self):
+        return Sentence.objects.filter(tags__name=self.name).distinct().count()
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "标签"
+        verbose_name_plural = verbose_name
